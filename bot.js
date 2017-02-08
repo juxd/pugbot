baf01bc82d2366b35305550be1df0e2f2d963138
ju <jluuiss@gmail.com>
2017-02-08 17:22:09 +0800

    saved for work

--- bot.js
+++ bot.js
@@ -1,67 +1,61 @@
+var fs = require('fs'); 
 var Discord = require("discord.js");
 var bot = new Discord.Client();
 var sqlite3 = require("sqlite3");
+var config = require("./config.json");
 var pugs = new sqlite3.Database("./db/pugs.db");
 var players = new sqlite3.Database("./db/players.db");
 
-let prefix = "!";
+let prefix = config.prefix;
 
-var pugInfo = [];
-var pugPlayers = [];
 var pugCount = 0;
+var pugLive = false;
+var pugPlayers = [];
+var entryCount = 0;
+var currentPugMsg = undefined;
 
-bot.on("message", msg => 
-{
-    if (msg.content.startsWith(prefix + "newpug")) 
-    {
-      if (!msg.author.bot && (msg.member.highestRole.position > 0)) 
-      {
-        pugInfo = msg.content.split(" ", 3);
-        pugCount++;
-        msg.channel.sendMessage("New pug (ID: " + pugCount.toString() + ") on " + pugInfo[1] + " at " + pugInfo[2]);
-        msg.channel.sendMessage("Type !join <id> to join");
-        pugs.run("INSERT into pugs VALUES(" + pugCount.toString() + ", '" + pugInfo[1] + "', '" + pugInfo[2] + ", 1)");
+bot.on("message", msg => {
+    if (msg.content.startsWith(prefix + "newpug")) {
+      if (!msg.author.bot && (msg.member.highestRole.position > 0)) {
+        if (pugLive == false) {
+          var pugInfo = msg.content.split(" ", 3);
+          pugCount++;
+          msg.channel.sendMessage("New pug on " + pugInfo[1] + " at " + pugInfo[2]);
+          msg.channel.sendMessage("Type !join to join")
+          pugs.run("INSERT into pugs VALUES(" + pugCount.toString() + ", '" + pugInfo[1] + "', '" + pugInfo[2] + "', 1)");
+        }
+        else if (pugLive == true){
+          msg.channel.sendMessage("A pug is ongoing!");
+        }
       };
     }
-    else if (msg.content.startsWith(prefix + "checkStat")) 
-    {
-      if (!msg.author.bot && (msg.member.highestRole.position > 0)) 
-      {
-         for (i=0; i<=pugPlayers.length; i++)
-         {
-             msg.channel.sendMessage(pugPlayers[i]);
-         }
-
-        msg.channel.sendMessage("Current list: \n" + pugPlayers.join("\n") + "");
+    else if (msg.content.startsWith(prefix + "checkStatus")) {
+      if (!msg.author.bot && (msg.member.highestRole.position > 0)) {
+        msg.channel.sendMessage("Current list: \n `" + pugPlayers.join("\n") + "`");
       };
     }
-    else if (msg.content.startsWith(prefix + "join")) 
-    {
-      if (!msg.author.bot) 
-      {
+    else if (msg.content.startsWith(prefix + "join")) {
+      if (!msg.author.bot && pugLive == true) {
         pugPlayers.push(msg.author.username);
-        msg.channel.sendMessage(pugPlayers.toString());
+        entryCount++;
+        pugs.run("INSERT into entries VALUES(" + entryCount.toString() + ", " + pugCount.toString() + ", '" + msg.author.username + "')");
         msg.channel.sendMessage(msg.author.username + ", you've been added to the list!");
         msg.delete;
-        msg.channel.sendMessage(pugPlayers.toString());
       };
     }
-    else if (msg.content.startsWith(prefix + "endpug")) 
-    {
-      if (!msg.author.bot && (msg.member.highestRole.position > 0)) 
-      {
-        pugs.run("UPDATE pugs SET live = 0 WHERE id = " + msg.content.match("\d"));
+    else if (msg.content.startsWith(prefix + "endpug")) {
+      if (!msg.author.bot && (msg.member.highestRole.position > 0)) {
+        pugPlayers = [];
+        pugLive = false;
       };
     }
-    else 
-    {
+    else {
 
     }
 });
 
-bot.on('ready', () => 
-{
+bot.on('ready', () => {
   console.log('I am ready!');
 });
 
-bot.login("<botkey>");
+bot.login(config.token);
